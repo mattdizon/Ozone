6fdb12d4e854e09e211845b5774b5d1cdf8e45c9
Matthew Dizon <matthewdizon94@protonmail.com>
2021-02-18 16:34:42 -0600

    fixed tests

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,15 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileTransformer.js"
   },
-  transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native)'],
+
   coveragePathIgnorePatterns: ['/node_modules/', '/jest'],
   testEnvironment: 'jsdom',
+  setupFiles: [
+    "./node_modules/react-native-gesture-handler/jestSetup.js"
+],
+transformIgnorePatterns: [
+    "/node_modules/(?!native-base)/"
+]
 };
